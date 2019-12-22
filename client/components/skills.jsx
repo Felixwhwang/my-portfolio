c76ba0552e598694489a3a39e6c814a31af89cd1
wenhao wang <felixwhwang@gmail.com>
2019-12-22 00:23:37 +0000

    menu bar finished

--- client/components/skills.jsx
+++ client/components/skills.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Skill() {
   return (
-    <h1 className="full-view" id="skills">Skills</h1>
+    <div className="full-view" id="skills">Skills</div>
   );
 }
