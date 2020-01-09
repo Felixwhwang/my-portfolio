2ce2a9792c8edb7dcbaf486b88cb472f32ebe9f4
wenhao wang <felixwhwang@gmail.com>
2020-01-09 01:17:04 +0000

    add one project demo

--- client/components/projects.jsx
+++ client/components/projects.jsx
@@ -76,6 +76,42 @@ export default function Projects() {
               </div>
             </div>
           </div>
+
+          <div className="col-lg-4 col-md-6 p-2">
+            <div className="card project-shadow h-100">
+              <div className="overflow-hidden">
+                <a href="https://soulmate.wenhaowang.net/" target="_blank"
+                  rel="noopener noreferrer">
+                  <img src="images/soulmate.png" className="img-fluid grow" />
+                </a>
+              </div>
+              <div className="card-body">
+                <div className="d-flex justify-content-between">
+                  <a href="https://soulmate.wenhaowang.net/" target="_blank"
+                    rel="noopener noreferrer">
+                    <h4 className="card-title d-inline">Soulmate Dating App</h4>
+                  </a>
+                  <div>
+                    <a href="https://github.com/Felixwhwang/soul-mate" target="_blank"
+                      rel="noopener noreferrer">
+                      <i className="fab fa-github project-icon git-color"></i>
+                    </a>
+                    <a href="https://soulmate.wenhaowang.net/" target="_blank"
+                      rel="noopener noreferrer">
+                      <i className="fab fa-chrome project-icon resume-color"></i>
+                    </a>
+                  </div>
+                </div>
+                <label className="text-muted">Full Stack App</label>
+                <p className="card-text">A web application for young people,
+                  and who want to find the ideal other half in his/her life.</p>
+                <p className="card-text">App front-end was built using <strong>React</strong>,
+                  <strong>HTML5</strong>, <strong>CSS3</strong>, and <strong>Bootstrap4</strong>.
+                Back-end was using <strong>MySQL</strong> database and <strong>PHP</strong>.</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
